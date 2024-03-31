21cc43fdf460cec8f1a032a39552f9abf22516f1
Nate Maninger <me@n8m.us>
2024-03-30 20:30:39 -0700

    currency: fix encoding, move siacoins up

--- src/currency.rs
+++ src/currency.rs
@@ -21,6 +21,23 @@ impl Currency {
 	pub fn new(value: u128) -> Self {
 		Currency(value)
 	}
+
+	/// Converts a given amount of Siacoins into the `Currency` type.
+	///
+	/// This function takes the amount of Siacoins as a `u64` and converts it into
+	/// the `Currency` type, which internally represents the value in Hastings where
+	/// 1 SC = 10^24 H.
+	///
+	/// # Arguments
+	///
+	/// * `n` - The amount of Siacoins to be converted into `Currency`.
+	///
+	/// # Returns
+	///
+	/// Returns a `Currency` instance representing the specified amount of Siacoins.
+	pub fn siacoins(n: u64) -> Self {
+		Currency::new((n as u128) * 10u128.pow(SIACOIN_PRECISION_U32))
+	}
 }
 
 impl Add for Currency {
@@ -61,7 +78,7 @@ impl SiaEncodable for Currency {
 		let i = currency_buf.iter()
         	.enumerate()
         	.find(|&(_index, &value)| value != 0)
-        	.map_or(0, |(index, _value)| index); // Use 0 if all values are zero
+        	.map_or(16, |(index, _value)| index); // 16 if all bytes are 0
 		
 		buf.extend_from_slice(&currency_buf[i..].len().to_le_bytes());
 		buf.extend_from_slice(&currency_buf[i..]);
@@ -169,23 +186,6 @@ impl FromStr for Currency {
 	}
 }
 
-/// Converts a given amount of Siacoins into the `Currency` type.
-///
-/// This function takes the amount of Siacoins as a `u64` and converts it into
-/// the `Currency` type, which internally represents the value in Hastings where
-/// 1 SC = 10^24 H.
-///
-/// # Arguments
-///
-/// * `n` - The amount of Siacoins to be converted into `Currency`.
-///
-/// # Returns
-///
-/// Returns a `Currency` instance representing the specified amount of Siacoins.
-pub fn siacoins(n: u64) -> Currency {
-	Currency::new((n as u128) * 10u128.pow(SIACOIN_PRECISION_U32))
-}
-
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -197,27 +197,27 @@ mod tests {
 			(Currency::new(100), "100 H"),
 			(Currency::new(1001), "1001 H"),
 			(Currency::new(10000), "10000 H"),
-			(siacoins(1)/Currency::new(1_000_000_000_000), "1 pS"),
-			(siacoins(151212312)/Currency::new(1_000_000_000_000), "151.212312 uS"),
-			(siacoins(1)/Currency::new(2), "500 mS"),
-			(siacoins(1), "1 SC"),
-			(siacoins(10), "10 SC"),
-			(siacoins(100), "100 SC"),
-			(siacoins(1000), "1 KS"),
-			(siacoins(10000), "10 KS"),
-			(siacoins(u16::MAX as u64), "65.535 KS"),
-			(siacoins(10_0000), "100 KS"),
-			(siacoins(1_000_000), "1 MS"),
-			(siacoins(10_000_000), "10 MS"),
-			(siacoins(100_000_000), "100 MS"),
-			(siacoins(1_000_000_000), "1 GS"),
-			(siacoins(u32::MAX as u64), "4.294967295 GS"),
-			(siacoins(10_000_000_000), "10 GS"),
-			(siacoins(100_000_000_000), "100 GS"),
-			(siacoins(1_000_000_000_000), "1 TS"),
-			(siacoins(10_000_000_000_000), "10 TS"),
-			(siacoins(100_000_000_000_000), "100 TS"),
-			(siacoins(10) - Currency::new(1), "9.999999999999999999999999 SC"),
+			(Currency::siacoins(1)/Currency::new(1_000_000_000_000), "1 pS"),
+			(Currency::siacoins(151212312)/Currency::new(1_000_000_000_000), "151.212312 uS"),
+			(Currency::siacoins(1)/Currency::new(2), "500 mS"),
+			(Currency::siacoins(1), "1 SC"),
+			(Currency::siacoins(10), "10 SC"),
+			(Currency::siacoins(100), "100 SC"),
+			(Currency::siacoins(1000), "1 KS"),
+			(Currency::siacoins(10000), "10 KS"),
+			(Currency::siacoins(u16::MAX as u64), "65.535 KS"),
+			(Currency::siacoins(10_0000), "100 KS"),
+			(Currency::siacoins(1_000_000), "1 MS"),
+			(Currency::siacoins(10_000_000), "10 MS"),
+			(Currency::siacoins(100_000_000), "100 MS"),
+			(Currency::siacoins(1_000_000_000), "1 GS"),
+			(Currency::siacoins(u32::MAX as u64), "4.294967295 GS"),
+			(Currency::siacoins(10_000_000_000), "10 GS"),
+			(Currency::siacoins(100_000_000_000), "100 GS"),
+			(Currency::siacoins(1_000_000_000_000), "1 TS"),
+			(Currency::siacoins(10_000_000_000_000), "10 TS"),
+			(Currency::siacoins(100_000_000_000_000), "100 TS"),
+			(Currency::siacoins(10) - Currency::new(1), "9.999999999999999999999999 SC"),
 			(Currency::new(50_587_566_000_000_000_000_000_000),"50.587566 SC"),
 			(Currency::new(2529378333356156158367), "2.529378333356156158367 mS"),
 			(Currency::new(u128::MAX), "340.282366920938463463374607431768211455 TS"),
@@ -235,33 +235,50 @@ mod tests {
 			("100 H", Currency::new(100)),
 			("1001 H", Currency::new(1001)),
 			("10000 H", Currency::new(10000)),
-			("1 pS", siacoins(1)/Currency::new(1_000_000_000_000)),
-			("151.212312 uS", siacoins(151212312)/Currency::new(1_000_000_000_000)),
-			("500 mS", siacoins(1)/Currency::new(2)),
-			("1 SC", siacoins(1)),
-			("10 SC", siacoins(10)),
-			("100 SC", siacoins(100)),
-			("1 KS", siacoins(1000)),
-			("10 KS", siacoins(10000)),
-			("65.535 KS", siacoins(u16::MAX as u64)),
-			("100 KS", siacoins(100000)),
-			("1 MS", siacoins(1000000)),
-			("10 MS", siacoins(10000000)),
-			("100 MS", siacoins(100000000)),
-			("1 GS", siacoins(1000000000)),
-			("4.294967295 GS", siacoins(u32::MAX as u64)),
-			("10 GS", siacoins(10000000000)),
-			("100 GS", siacoins(100000000000)),
-			("1 TS", siacoins(1000000000000)),
-			("10 TS", siacoins(10000000000000)),
-			("100 TS", siacoins(100000000000000)),
-			("9.999999999999999999999999 SC", siacoins(10) - Currency::new(1)),
+			("1 pS", Currency::siacoins(1)/Currency::new(1_000_000_000_000)),
+			("151.212312 uS", Currency::siacoins(151212312)/Currency::new(1_000_000_000_000)),
+			("500 mS", Currency::siacoins(1)/Currency::new(2)),
+			("1 SC", Currency::siacoins(1)),
+			("10 SC", Currency::siacoins(10)),
+			("100 SC", Currency::siacoins(100)),
+			("1 KS", Currency::siacoins(1000)),
+			("10 KS", Currency::siacoins(10000)),
+			("65.535 KS", Currency::siacoins(u16::MAX as u64)),
+			("100 KS", Currency::siacoins(100000)),
+			("1 MS", Currency::siacoins(1000000)),
+			("10 MS", Currency::siacoins(10000000)),
+			("100 MS", Currency::siacoins(100000000)),
+			("1 GS", Currency::siacoins(1000000000)),
+			("4.294967295 GS", Currency::siacoins(u32::MAX as u64)),
+			("10 GS", Currency::siacoins(10000000000)),
+			("100 GS", Currency::siacoins(100000000000)),
+			("1 TS", Currency::siacoins(1000000000000)),
+			("10 TS", Currency::siacoins(10000000000000)),
+			("100 TS", Currency::siacoins(100000000000000)),
+			("9.999999999999999999999999 SC", Currency::siacoins(10) - Currency::new(1)),
 			("50.587566 SC", Currency::new(50587566000000000000000000)),
 			("2.529378333356156158367 mS", Currency::new(2529378333356156158367)),
 			("340.282366920938463463374607431768211455 TS", Currency::new(u128::MAX)),
 		];
 		for (input, expected) in test_cases {
 			assert_eq!(input.parse::<Currency>().unwrap(), expected);
+		}
+	}
+
+	#[test]
+	fn test_encode() {
+		let test_cases = vec![
+			(Currency::new(0),vec![0,0,0,0,0,0,0,0]),
+			(Currency::new(10000),vec![2,0,0,0,0,0,0,0,39,16]),
+			(Currency::new(50587566000000000000000000),vec![11,0,0,0,0,0,0,0,41,216,85,114,169,108,83,1,248,0,0]),
+			(Currency::new(2529378333356156158367),vec![9,0,0,0,0,0,0,0,137,30,45,0,247,119,66,129,159]),
+			(Currency::new(340282366920938463463374607431768211455),vec![16,0,0,0,0,0,0,0,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255]),
+		];
+
+		for (currency, expected) in test_cases {
+			let mut buf = Vec::new();
+			currency.encode(&mut buf);
+			assert_eq!(buf, expected, "failed for {:?}", currency);
 		}
 	}
 }
