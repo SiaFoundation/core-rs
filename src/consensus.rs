f3373a1d117d574a93519c7383dce542995bbe75
Nate Maninger <me@n8m.us>
2024-03-31 20:39:26 -0700

    add display format

--- src/consensus.rs
+++ src/consensus.rs
@@ -1,4 +1,12 @@
+use std::fmt;
+
 pub struct ChainIndex {
 	pub height: u64,
 	pub id: [u8; 32],
+}
+
+impl fmt::Display for ChainIndex {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}:{}", self.height, hex::encode(&self.id))
+	}
 }
