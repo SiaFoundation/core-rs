d37d7c76914a3816ffc20e75f934dcf02ecdd52e
Chris Schinnerl <chris@sia.tech>
2024-04-15 13:11:56 +0200

    encoding: rustfmt

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,8 +1,11 @@
-use serde::{ser::{self, SerializeSeq}, Serialize};
+use byteorder::{LittleEndian, WriteBytesExt};
+use serde::{
+    ser::{self, SerializeSeq},
+    Serialize,
+};
+use std::fmt::Display;
 use std::io;
 use thiserror::Error;
-use std::fmt::Display;
-use byteorder::{LittleEndian, WriteBytesExt};
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -16,12 +19,15 @@ pub enum Error {
     LengthUnavailable,
 
     #[error("Failed to write to io::write: {0}")]
-    IO(#[from]io::Error),
+    IO(#[from] io::Error),
 }
 
 // Implement ser::Error for Error
 impl ser::Error for Error {
-    fn custom<T>(msg:T) -> Self where T:Display {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: Display,
+    {
         Error::Custom(msg.to_string())
     }
 }
@@ -35,27 +41,29 @@ where
     Ok(buf)
 }
 
-pub fn to_writer<T>(writer: &mut dyn io::Write, value: &T) -> Result<(), Error>
+pub fn to_writer<T, W>(writer: &mut W, value: &T) -> Result<(), Error>
 where
     T: Serialize,
+    W: io::Write,
 {
-    let mut serializer = Serializer {
-        writer: writer,
-    };
+    let mut serializer = Serializer { writer: writer };
     value.serialize(&mut serializer)
 }
 
-pub struct Serializer<'a> {
-    writer: &'a mut dyn io::Write,
+pub struct Serializer<'a, W>
+where
+    W: io::Write,
+{
+    writer: &'a mut W,
 }
 
-impl ser::Serializer for &mut Serializer<'_> {
+impl<W: io::Write> ser::Serializer for &mut Serializer<'_, W> {
     // Result is written to internal io::Write
     type Ok = ();
 
     // Error is a standard error
     type Error = Error;
-    
+
     // Associated types for keeping track of additional state while serializing
     // compound data structures like sequences and maps. In this case no
     // additional state is required beyond what is already stored in the
@@ -67,101 +75,102 @@ impl ser::Serializer for &mut Serializer<'_> {
     type SerializeMap = Self;
     type SerializeStruct = Self;
     type SerializeStructVariant = Self;
-    
+
     fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
         self.writer.write_u8(if v { 1 } else { 0 })?;
         Ok(())
     }
-    
+
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
         self.writer.write_i8(v)?;
         Ok(())
     }
-    
+
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
         self.writer.write_i16::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
         self.writer.write_i32::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
         self.writer.write_i64::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
         self.writer.write_u8(v)?;
         Ok(())
     }
-    
+
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
         self.writer.write_u16::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
         self.writer.write_u32::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
         self.writer.write_u64::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
         self.writer.write_f32::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
         self.writer.write_f64::<LittleEndian>(v)?;
         Ok(())
     }
-    
+
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
         let mut buf: [u8; 4] = [0; 4];
         self.serialize_str(v.encode_utf8(&mut buf))
     }
-    
+
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
         self.serialize_bytes(v.as_bytes())
     }
-    
+
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
         let seq = self.serialize_seq(Some(v.len()))?;
         seq.writer.write_all(v)?;
         seq.end()
     }
-    
+
     // 'none' is serialized as a '0' byte
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
         self.writer.write_all(&[1; 0])?;
         Ok(())
     }
-    
+
     // 'some' is serialized by writing a '1' byte followed by the value
     fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
         self.writer.write_all(&[1; 1])?;
         value.serialize(self)
     }
-    
+
     // units don't have a value so they are ignored
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
         Ok(())
     }
-    
+
     // unit_structs don't have a value so they are ignored
     fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
         Ok(())
     }
-    
+
     // newtype_variants are not supported
     fn serialize_unit_variant(
         self,
@@ -171,7 +180,7 @@ impl ser::Serializer for &mut Serializer<'_> {
     ) -> Result<Self::Ok, Self::Error> {
         Err(Error::UnsupportedType("unit_variant"))
     }
-    
+
     // As is done here, serializers are encouraged to treat newtype structs as
     // insignificant wrappers around the data they contain
     fn serialize_newtype_struct<T: ?Sized>(
@@ -180,10 +189,11 @@ impl ser::Serializer for &mut Serializer<'_> {
         value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
         value.serialize(self)
     }
-    
+
     // newtype_variants are not supported
     fn serialize_newtype_variant<T: ?Sized>(
         self,
@@ -193,10 +203,11 @@ impl ser::Serializer for &mut Serializer<'_> {
         _value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
-        T: Serialize {
+        T: Serialize,
+    {
         Err(Error::UnsupportedType("newtype_variant"))
     }
-    
+
     // sequences have an 8 byte, little-endian encoded prefix describing their
     // length
     fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
@@ -205,17 +216,15 @@ impl ser::Serializer for &mut Serializer<'_> {
                 self.writer.write_u64::<LittleEndian>(len as u64)?;
                 Ok(self)
             }
-            None => {
-                Err(Error::LengthUnavailable)
-            }
+            None => Err(Error::LengthUnavailable),
         }
     }
-    
+
     // serialize_tuple is called on fixed size arrays as well as tuples
     fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple, Self::Error> {
         Ok(self)
     }
-    
+
     // tuple structs are not supported
     fn serialize_tuple_struct(
         self,
@@ -224,7 +233,7 @@ impl ser::Serializer for &mut Serializer<'_> {
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
         Err(Error::UnsupportedType("tuple_struct"))
     }
-    
+
     // tuple variants are not supported
     fn serialize_tuple_variant(
         self,
@@ -235,12 +244,12 @@ impl ser::Serializer for &mut Serializer<'_> {
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
         Err(Error::UnsupportedType("tuple_variant"))
     }
-    
+
     // maps are not supported
     fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
         Err(Error::UnsupportedType("map"))
     }
-    
+
     // serializing a struct doesn't require any 'setup'
     fn serialize_struct(
         self,
@@ -249,7 +258,7 @@ impl ser::Serializer for &mut Serializer<'_> {
     ) -> Result<Self::SerializeStruct, Self::Error> {
         Ok(self)
     }
-    
+
     // struct variants are not supported
     fn serialize_struct_variant(
         self,
@@ -269,7 +278,7 @@ impl ser::Serializer for &mut Serializer<'_> {
 //
 // This impl is SerializeSeq so these methods are called after `serialize_seq`
 // is called on the Serializer.
-impl<'a> ser::SerializeSeq for &'a mut Serializer<'_> {
+impl<'a, W: io::Write> ser::SerializeSeq for &'a mut Serializer<'_, W> {
     // Must match the `Ok` type of the serializer.
     type Ok = ();
     // Must match the `Error` type of the serializer.
@@ -289,8 +298,8 @@ impl<'a> ser::SerializeSeq for &'a mut Serializer<'_> {
     }
 }
 
-// Tuples are not supported 
-impl<'a> ser::SerializeTuple for &'a mut Serializer<'_> {
+// Tuples are not supported
+impl<'a, W: io::Write> ser::SerializeTuple for &'a mut Serializer<'_, W> {
     type Ok = ();
     type Error = Error;
 
@@ -307,7 +316,7 @@ impl<'a> ser::SerializeTuple for &'a mut Serializer<'_> {
 }
 
 // Tuple structs are serialized like their inner value
-impl<'a> ser::SerializeTupleStruct for &'a mut Serializer<'_> {
+impl<'a, W: io::Write> ser::SerializeTupleStruct for &'a mut Serializer<'_, W> {
     type Ok = ();
     type Error = Error;
 
@@ -324,7 +333,7 @@ impl<'a> ser::SerializeTupleStruct for &'a mut Serializer<'_> {
 }
 
 // Tuple variants aren't supported by the Sia encoding
-impl<'a> ser::SerializeTupleVariant for &'a mut Serializer<'_> {
+impl<'a, W: io::Write> ser::SerializeTupleVariant for &'a mut Serializer<'_, W> {
     type Ok = ();
     type Error = Error;
 
@@ -341,7 +350,7 @@ impl<'a> ser::SerializeTupleVariant for &'a mut Serializer<'_> {
 }
 
 // Serializing maps is not supported
-impl<'a> ser::SerializeMap for &'a mut Serializer<'_> {
+impl<'a, W: io::Write> ser::SerializeMap for &'a mut Serializer<'_, W> {
     type Ok = ();
     type Error = Error;
 
@@ -376,7 +385,7 @@ impl<'a> ser::SerializeMap for &'a mut Serializer<'_> {
 }
 
 // Structs are serialized one field after another.
-impl<'a> ser::SerializeStruct for &'a mut Serializer<'_> {
+impl<'a, W: io::Write> ser::SerializeStruct for &'a mut Serializer<'_, W> {
     type Ok = ();
     type Error = Error;
 
@@ -393,11 +402,15 @@ impl<'a> ser::SerializeStruct for &'a mut Serializer<'_> {
 }
 
 // Struct variants are not supported
-impl<'a> ser::SerializeStructVariant for &'a mut Serializer<'_> {
+impl<'a, W: io::Write> ser::SerializeStructVariant for &'a mut Serializer<'_, W> {
     type Ok = ();
     type Error = Error;
 
-    fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<Self::Ok, Self::Error>
+    fn serialize_field<T>(
+        &mut self,
+        _key: &'static str,
+        _value: &T,
+    ) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + Serialize,
     {
@@ -423,8 +436,6 @@ fn test_struct() {
         seq: vec![1, 2, 3],
         arr: [1, 2, 3],
     };
-    let expected = [1, 0, 0, 0,
-                              3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3,
-                              1, 2, 3];
+    let expected = [1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 1, 2, 3];
     assert_eq!(to_bytes(&test).unwrap(), expected);
 }
