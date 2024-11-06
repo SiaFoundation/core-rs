b4a0a12d7c36240c2de8970be952f0a2cf84ffee
Nate Maninger <me@n8m.us>
2024-11-06 09:16:09 -0800

    encoding: fix derive location

--- sia/src/encoding.rs
+++ sia/src/encoding.rs
@@ -1,8 +1,8 @@
 mod v1_encoding;
 mod v2_encoding;
 
-pub use sia_derive::{SiaDecode, SiaEncode};
+pub use sia_sdk_derive::{SiaDecode, SiaEncode};
 pub use v2_encoding::{Error, Result, SiaDecodable, SiaEncodable};
 
-pub use sia_derive::{V1SiaDecode, V1SiaEncode};
+pub use sia_sdk_derive::{V1SiaDecode, V1SiaEncode};
 pub use v1_encoding::{V1SiaDecodable, V1SiaEncodable};
